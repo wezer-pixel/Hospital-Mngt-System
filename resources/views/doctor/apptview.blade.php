6a65cff479f0a9dcd369b195e8c3ce5865897264
wezer-pixel <jerryjuma104@gmail.com>
2023-08-26 19:09:54 +0300

    edit appt

--- resources/views/doctor/apptview.blade.php
+++ resources/views/doctor/apptview.blade.php
@@ -25,22 +25,12 @@
                       <p class="text-gray-700 text-2xl">
                         Hello
                       </p>
-                      <p class="text-gray-700 text-2xl">
-                        Hello
-                      </p>
-                      <p class="text-gray-700 text-2xl">
-                        Hello
-                      </p>
-                      <p class="text-gray-700 text-2xl">
-                        Hello
-                      </p>
-                      <p class="text-gray-700 text-2xl">
-                        Hello
-                      </p>
                       <a href="{{url('show_doctor')}}" class="mt-4 inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">View Available Doctors</a>
                     </div>
                   </div>
                 </div>
+
+
 
               </div>
         </div>
